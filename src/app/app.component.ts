217fddedcd7eb24a9f17df5ccc23706c04ed6978
bexposito <>
2024-08-03 09:31:22 +0200

    Added RibbonMenu to AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { RibbonMenuComponent } from './components/ribbon-menu/ribbon-menu.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RibbonMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'DataCleaner';
+  handleButtonClick(buttonId: string): void {
+    alert(`Button clicked: ${buttonId}`);
+  }
 }
