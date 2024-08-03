69997711e4a5b6c5483cb84cbf4fb7466b2fef03
bexposito <>
2024-08-03 07:38:06 +0200

    Added HeaderButton component

---
+++ src/app/components/header-button/header-button.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'header-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './header-button.component.html',
+  styleUrl: './header-button.component.css'
+})
+export class HeaderButtonComponent {
+  @Input() text: string = '';
+  @Input() icon: string = '';
+}
