3a11900448d8a8c68fc9674da1e7a7afc88dafbc
bexposito <>
2024-08-03 09:19:55 +0200

    RibbonMenu - Buttons - Added click event and an identifier

--- src/app/components/ribbon-menu/ribbon-menu.component.ts
+++ src/app/components/ribbon-menu/ribbon-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { HeaderButtonComponent } from './../header-button/header-button.component';
 import { CommonModule } from '@angular/common';
 
@@ -17,37 +17,47 @@ export class RibbonMenuComponent {
       title: 'Import',
       items: [{
         icon: 'fa-solid fa-file-csv',
-        text: 'CSV'
+        text: 'CSV',
+        id: 'import_csv'
       },{
         icon: 'fa-brands fa-html5',
-        text: 'HTML'
+        text: 'HTML',
+        id: 'import_html'
       },{
         icon: 'fa-solid fa-code',
-        text: 'XML'
+        text: 'XML',
+        id: 'import_xml'
       },{
         icon: 'fa-solid fa-file',
-        text: 'JSON'
+        text: 'JSON',
+        id: 'import_json'
       },{
         icon: 'fa-solid fa-database',
-        text: 'SQL'
+        text: 'SQL',
+        id: 'import_sqll'
       }]
     },{
       title: 'Export',
       items: [{
         icon: 'fa-solid fa-file-csv',
-        text: 'CSV'
+        text: 'CSV',
+        id: 'export_csv'
       },{
         icon: 'fa-brands fa-html5',
-        text: 'HTML'
+        text: 'HTML',
+        id: 'export_html'
       },{
         icon: 'fa-solid fa-code',
-        text: 'XML'
+        text: 'XML',
+        id: 'export_xml'
       },{
         icon: 'fa-solid fa-file',
-        text: 'JSON'
+        text: 'JSON',
+        id: 'export_json'
       },{
         icon: 'fa-solid fa-database',
-        text: 'SQL'
+        text: 'SQL',
+        id: 'export_sql'
       }]
     }]
   },{
@@ -57,32 +67,45 @@ export class RibbonMenuComponent {
       title: 'Add column',
       items: [{
         icon: 'fa-solid fa-angles-left',
-        text: 'Start'
+        text: 'Start',
+        id: 'add_column_start'
       },{
         icon: 'fa-solid fa-chevron-left',
-        text: 'Left'
+        text: 'Left',
+        id: 'add_column_left'
       },{
         icon: 'fa-solid fa-chevron-right',
-        text: 'Right'
+        text: 'Right',
+        id: 'add_column_right'
       },{
         icon: 'fa-solid fa-angles-right',
-        text: 'End'
+        text: 'End',
+        id: 'add_column_end'
       }]
     },{
       title: 'Add row',
       items: [{
         icon: 'fa-solid fa-angles-up',
-        text: 'Start'
+        text: 'Start',
+        id: 'add_row_start'
       },{
         icon: 'fa-solid fa-chevron-up',
-        text: 'Up'
+        text: 'Up',
+        id: 'add_row_up'
       },{
         icon: 'fa-solid fa-chevron-down',
-        text: 'Down'
+        text: 'Down',
+        id: 'add_row_down'
       },{
         icon: 'fa-solid fa-angles-down',
-        text: 'End'
+        text: 'End',
+        id: 'add_row_end'
       }]
     }]
   }];
+
+  @Output() buttonClick = new EventEmitter<string>();
+  onButtonClick(id: string): void {
+    this.buttonClick.emit(id);
+  }
 }
